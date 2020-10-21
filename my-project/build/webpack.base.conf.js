bedcebc9d53b3f85bf6c28841a37b3788bdda266
yjghsjz <yjghsjz@github.com>
2020-10-21 08:29:09 +0800

    添加登录页面

--- my-project/build/webpack.base.conf.js
+++ my-project/build/webpack.base.conf.js
@@ -27,6 +27,7 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+      'page': resolve('src/page')
     }
   },
   module: {
--- my-project/src/router/index.js
+++ my-project/src/router/index.js
@@ -3,17 +3,22 @@ import Router from 'vue-router'
 import Layout from '@/page/layout'
 import Main from '@/page/main'
 import User from '@/page/user'
+import Login from '@/page/login'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
+  routes: [{
+      path: '/',
+      name: 'Login',
+      component: Login
+    },
     {
       path: '/',
       name: 'Layout',
       component: Layout,
       children: [{
         // 这里不设置值，是把main作为默认页面
-        path: '/', 
+        path: '/main', 
         name: 'Main',
         component: Main
       },{
